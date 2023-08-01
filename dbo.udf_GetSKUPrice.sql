895ec457c3d280c3705a9a1870bbaa14b56bd6c9
TolikOborin37 <tolik.oborin.1996@gmail.com>
2023-08-01 07:12:42 +0400

    файл в репозитории dbo.udf_GetSKUPrice.sql в ветке Functions

---
+++ dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,9 @@
+create function GetSKUPrice(@ID_SKU AS int)
+Returns decimal(18,1)
+Begin
+Declare @Summ decimal(18,2)
+Select @Summ = CAST(SUM(Value) AS decimal(18,2))/CAST(SUM(Quantity) AS decimal(18,2))
+from dbo.Basket
+Where @ID_SKU = ID_SKU
+Return @Summ
+end
